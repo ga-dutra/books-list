3ca3a883708b425895599969d57a7fc44ca3d2f9
gacdutra <ga.acdutra@gmail.com>
2022-11-13 19:48:13 -0300

    feat: added book and movie schemas

---
+++ src/schemas/books.schemas.js
@@ -0,0 +1,18 @@
+import Joi from "joi";
+
+export const BookSchema = Joi.object({
+  title: Joi.string().required(),
+  author: Joi.string().required(),
+  read: Joi.boolean(),
+  score: Joi.number(),
+  overview: Joi.string().required(),
+});
+
+export const MovieSchema = Joi.object({
+  title: Joi.string().required(),
+  platform: Joi.string().required(),
+  genre: Joi.string().required(),
+  watched: Joi.boolean(),
+  score: Joi.number(),
+  overview: Joi.string().required(),
+});
