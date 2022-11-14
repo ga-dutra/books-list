c499316c626a5f88687022adce016011aad26197
gacdutra <ga.acdutra@gmail.com>
2022-11-14 09:14:34 -0300

    feat: added cors dependency

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import booksRouter from "./routes/books.routes.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()).use(cors());
 app.use(booksRouter);
 
 dotenv.config();
