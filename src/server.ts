953314496abebc80ab971f0e9d7e02752565863a
gacdutra <ga.acdutra@gmail.com>
2022-11-14 09:52:47 -0300

    prepared to deploy

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import booksRouter from "./routes/books.routes.js";
 
+dotenv.config();
+
 const app = express();
 
 app.use(express.json()).use(cors());
-app.use(booksRouter);
-
-dotenv.config();
+app.use("", booksRouter);
 
 const PORT: number = Number(process.env.PORT) || 4000;
 
