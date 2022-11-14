60658291937f4e60e587694107e3696125315bbe
gacdutra <ga.acdutra@gmail.com>
2022-11-14 08:57:41 -0300

    prepared to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 import booksRouter from "./routes/books.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(booksRouter);
+
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT) || 4000;
 
 app.listen(4000, () => {
   console.log("Listening on port 4000");
